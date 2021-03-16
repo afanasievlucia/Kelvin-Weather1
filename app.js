1fc8fb6451df230467e93015d5ea38b005de10c6
afanasievlucia <78347720+afanasievlucia@users.noreply.github.com>
2021-03-16 15:19:08 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const Kelvin = 293; // a constant named kelvin with 293 value.
+const Celsius = Kelvin - 273; // constant Celsius smaller with 273 then Kelvin.
+let Fahrenheit = Celsius * (9/5) + 32; // a variable type that can change its value.
+Fahrenheit = (Math.floor(Fahrenheit)); // a new value for variable Fahrenheit that round down its decimal number value.
+console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
